216ff331f5ab9ffaca441e989d6bf4a449d539f2
Taeyong Kim <tk33@kay.cs.calvin.edu>
2017-10-27 15:31:37 -0400

    initial push commit thing

---
+++ back-end/routes/scripts.js
@@ -0,0 +1,22 @@
+/*
+ * scripts.js Maintains routing information for script handling
+ */
+
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+	// Normally you'd call a function (like I do in files.js)
+	res.json([{
+		id: 1,
+		username: "samsepi0l"
+	}, {
+		id: 2,
+		username: "D0loresH4ze"
+	}]);
+});
+
+module.exports = router;
